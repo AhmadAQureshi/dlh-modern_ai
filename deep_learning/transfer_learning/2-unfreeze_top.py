4f1a4c958d39a2319dc427c81f2b3fc7658a3cdc
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-09-09 00:56:18 +0200

    Refactor unfreeze_top_layers to access base model
    Updated the unfreeze_top_layers function to target the base model's layers directly.

--- deep_learning/transfer_learning/2-unfreeze_top.py
+++ deep_learning/transfer_learning/2-unfreeze_top.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-"""Utilities for unfreezing top layers of a pretrained model."""
+"""Unfreeze selected top layers of a pretrained base model."""
 
 
 def unfreeze_top_layers(model, n_layers):
-    """Unfreeze the last n_layers of model and keep earlier layers frozen."""
-    model.trainable = True
+    """Unfreeze the last n layers of the base model."""
+    base_model = model.layers[1]
 
-    for layer in model.layers[:-n_layers]:
+    for layer in base_model.layers[:-n_layers]:
         layer.trainable = False
 
-    for layer in model.layers[-n_layers:]:
+    for layer in base_model.layers[-n_layers:]:
         layer.trainable = True
